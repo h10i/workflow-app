6a9c9125442c0e8a12365142408ba1cc7fcc202a
hiro <43287732+h10i@users.noreply.github.com>
2025-05-13 20:47:53 +0900

    feat: add a new Spring Boot project

---
+++ src/main/kotlin/com/example/workflow/WorkflowApplication.kt
@@ -0,0 +1,11 @@
+package com.example.workflow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WorkflowApplication
+
+fun main(args: Array<String>) {
+	runApplication<WorkflowApplication>(*args)
+}
