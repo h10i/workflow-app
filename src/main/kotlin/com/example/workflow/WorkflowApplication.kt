b2bfb06dc1c77d552aa7c438809e745ffb7aaa4d
hiro <43287732+h10i@users.noreply.github.com>
2025-06-09 22:40:42 +0900

    feat: configure OpenAPI documentation with server environments

--- src/main/kotlin/com/example/workflow/WorkflowApplication.kt
+++ src/main/kotlin/com/example/workflow/WorkflowApplication.kt
@@ -1,12 +1,36 @@
 package com.example.workflow
 
 import com.example.workflow.infra.security.model.RsaKeyProperties
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 @EnableConfigurationProperties(RsaKeyProperties::class)
+@OpenAPIDefinition(
+    info = Info(
+        title = "workflow-app API",
+        version = "1.0",
+        description = "",
+    ),
+    servers = [
+        Server(
+            description = "development",
+            url = "http://development.eaxmple.com",
+        ),
+        Server(
+            description = "staging",
+            url = "http://staging.eaxmple.com",
+        ),
+        Server(
+            description = "production",
+            url = "http://production.eaxmple.com",
+        ),
+    ]
+)
 class WorkflowApplication
 
 fun main(args: Array<String>) {
