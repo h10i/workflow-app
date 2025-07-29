27519bb0976a6f1cc1624d8f136e2fbabb121ccd
hiro <43287732+h10i@users.noreply.github.com>
2025-07-29 20:52:19 +0900

    feat(account): add RegisterAccountUseCase

---
+++ src/main/kotlin/com/example/workflow/feature/account/usecase/RegisterAccountUseCase.kt
@@ -0,0 +1,27 @@
+package com.example.workflow.feature.account.usecase
+
+import com.example.workflow.core.account.Account
+import com.example.workflow.feature.account.model.AccountViewDto
+import com.example.workflow.feature.account.model.RegisterAccountRequest
+import com.example.workflow.feature.account.service.AccountService
+import org.springframework.stereotype.Service
+
+@Service
+class RegisterAccountUseCase(
+    private val accountService: AccountService,
+) {
+    data class Result(
+        val accountViewDto: AccountViewDto
+    )
+
+    fun execute(request: RegisterAccountRequest): Result {
+        val account = Account(
+            emailAddress = request.emailAddress,
+            password = request.password,
+        )
+        val accountViewDto: AccountViewDto = accountService.saveAccount(account)
+        return Result(
+            accountViewDto = accountViewDto,
+        )
+    }
+}
---
+++ src/test/kotlin/com/example/workflow/unit/feature/account/usecase/RegisterAccountUseCaseTest.kt
@@ -0,0 +1,58 @@
+package com.example.workflow.unit.feature.account.usecase
+
+import com.example.workflow.core.account.Account
+import com.example.workflow.feature.account.model.AccountViewDto
+import com.example.workflow.feature.account.model.RegisterAccountRequest
+import com.example.workflow.feature.account.service.AccountService
+import com.example.workflow.feature.account.usecase.RegisterAccountUseCase
+import com.example.workflow.support.annotation.UnitTest
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+@UnitTest
+class RegisterAccountUseCaseTest {
+    private lateinit var accountServiceMock: AccountService
+    private lateinit var registerAccountUseCase: RegisterAccountUseCase
+
+    @BeforeEach
+    fun setUp() {
+        accountServiceMock = mockk()
+        registerAccountUseCase = RegisterAccountUseCase(accountServiceMock)
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Nested
+    inner class ExecuteMethod {
+        @Test
+        fun `execute method should return account`() {
+            // Arrange
+            val request = RegisterAccountRequest(
+                emailAddress = "user@example.com",
+                password = "test-password",
+            )
+            val accountViewDto: AccountViewDto = mockk()
+            val claimsSlot = slot<Account>()
+
+            every { accountServiceMock.saveAccount(capture(claimsSlot)) } returns accountViewDto
+
+            // Act
+            val actual: RegisterAccountUseCase.Result = registerAccountUseCase.execute(request)
+
+            // Assert
+            val claimsSet = claimsSlot.captured
+            assertEquals(request.emailAddress, claimsSet.emailAddress)
+            assertEquals(request.password, claimsSet.password)
+
+            assertEquals(accountViewDto, actual.accountViewDto)
+        }
+    }
+}
