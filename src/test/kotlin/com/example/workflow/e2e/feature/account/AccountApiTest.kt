09ec4a0f85725c3a63bc6fdcd47caf421b0e8b81
hiro <43287732+h10i@users.noreply.github.com>
2025-07-30 22:10:24 +0900

    feat(account): add account registration E2E tests to AccountApiTest

--- src/test/kotlin/com/example/workflow/e2e/feature/account/AccountApiTest.kt
+++ src/test/kotlin/com/example/workflow/e2e/feature/account/AccountApiTest.kt
@@ -4,20 +4,105 @@ import com.example.workflow.common.path.ApiPath
 import com.example.workflow.e2e.test.base.AbstractE2ETest
 import com.example.workflow.e2e.test.web.client.E2ETestRestTemplate
 import com.example.workflow.support.annotation.E2ETest
+import com.fasterxml.jackson.databind.node.ObjectNode
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
-import kotlin.test.assertNull
+import java.util.*
+import kotlin.test.*
 
 @E2ETest
 class AccountApiTest(
     @Autowired
     private val restTemplate: E2ETestRestTemplate
 ) : AbstractE2ETest() {
+    @Nested
+    inner class RegisterAccount {
+        @Test
+        fun `POST valid request returns 201 Created`() {
+            // Arrange
+            val json = """
+            {
+              "emailAddress": "user001@example.com",
+              "password": "P4sSw0rd!"
+            }
+            """.trimIndent()
+
+            // Act
+            val response = restTemplate.post(
+                responseType = String::class.java,
+                path = ApiPath.Account.BASE,
+                body = json,
+            )
+
+            // Assert
+            assertEquals(HttpStatus.CREATED, response.statusCode)
+            assertNotNull(response.body)
+            val mapper = jacksonObjectMapper()
+            val actualBody = mapper.readTree(response.body)
+            val actualIdNode = actualBody["id"]
+            assertNotNull(actualIdNode)
+            assertTrue(actualIdNode.isTextual)
+            try {
+                UUID.fromString(actualIdNode.asText())
+            } catch (_: IllegalArgumentException) {
+                fail("ID is not a valid UUID format: ${actualIdNode.asText()}")
+            }
+
+            val actualBodyWithoutId: ObjectNode = (actualBody as ObjectNode).deepCopy()
+            actualBodyWithoutId.remove("id")
+
+            val expectedBody = mapper.readTree(
+                """
+                {
+                    "emailAddress":"user001@example.com",
+                    "roleNames":[]
+                }
+                """
+            )
+
+            assertEquals(expectedBody, actualBodyWithoutId)
+        }
+
+        @Test
+        fun `POST invalid request returns 400 Bad Request when email address is already registered`() {
+            // Arrange
+            val json = """
+            {
+              "emailAddress": "test@example.com",
+              "password": "P4sSw0rd!"
+            }
+            """.trimIndent()
+
+            // Act
+            val response = restTemplate.post(
+                responseType = String::class.java,
+                path = ApiPath.Account.BASE,
+                body = json,
+            )
+
+            // Assert
+            assertEquals(HttpStatus.BAD_REQUEST, response.statusCode)
+            assertNotNull(response.body)
+            val mapper = jacksonObjectMapper()
+            val expectedBody = mapper.readTree(
+                """
+                {
+                    "errors": {
+                        "emailAddress": [
+                            "This email address is already registered."
+                        ]
+                    }
+                }
+                """
+            )
+            val actualBody = mapper.readTree(response.body)
+            assertEquals(expectedBody, actualBody)
+        }
+    }
+
     @Nested
     inner class GetAccount {
         @Test
@@ -48,7 +133,6 @@ class AccountApiTest(
             val actualBody = mapper.readTree(response.body)
             assertEquals(expectedBody, actualBody)
         }
-
 
         @Test
         fun `GET account with invalid credentials returns 401 Unauthorize`() {
