776e54d7a0cfd290d76c46ace738c249f4e200d8
hiro <43287732+h10i@users.noreply.github.com>
2025-08-06 23:56:02 +0900

    chore(account): apply @get:Schema to DTO properties

--- src/main/kotlin/com/example/workflow/feature/account/model/RegisterAccountRequest.kt
+++ src/main/kotlin/com/example/workflow/feature/account/model/RegisterAccountRequest.kt
@@ -7,14 +7,14 @@ import jakarta.validation.constraints.NotBlank
 data class RegisterAccountRequest(
     @field:NotBlank(message = "Email address must not be blank")
     @field:Email(message = "Invalid email address format")
-    @Schema(
+    @get:Schema(
         description = "your email address",
         example = "user@example.com",
     )
     val emailAddress: String,
 
     @field:NotBlank(message = "password must not be blank")
-    @Schema(
+    @get:Schema(
         description = "Your account password",
         example = "P4sSw0rd!",
     )
