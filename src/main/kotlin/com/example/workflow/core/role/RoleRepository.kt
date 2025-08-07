867a3552152aad2b5fa952d07bf6aa54cc5f1629
hiro <43287732+h10i@users.noreply.github.com>
2025-08-07 23:55:17 +0900

    feat(role): add function to find by name in RoleRepository

--- src/main/kotlin/com/example/workflow/core/role/RoleRepository.kt
+++ src/main/kotlin/com/example/workflow/core/role/RoleRepository.kt
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
 interface RoleRepository : JpaRepository<Role, UUID> {
+    fun findByName(name: String): Role?
 }
--- src/test/kotlin/com/example/workflow/integration/core/role/RoleRepositoryTest.kt
+++ src/test/kotlin/com/example/workflow/integration/core/role/RoleRepositoryTest.kt
@@ -12,10 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 @IntegrationTest
 @DataJpaTest
-
 class RoleRepositoryTest {
     @Autowired
     private lateinit var entityManager: TestEntityManager
@@ -43,6 +43,49 @@ class RoleRepositoryTest {
 
             // Assert
             assertEquals(role, actual)
+        }
+    }
+
+    @Nested
+    inner class FindByName {
+        private lateinit var role: Role
+
+        @BeforeEach
+        fun setUp() {
+            // Arrange
+            role = TestDataFactory.createRole()
+            entityManager.persist(role)
+
+            entityManager.flush()
+            entityManager.clear()
+        }
+
+        @AfterEach
+        fun tearDown() {
+        }
+
+        @Test
+        fun `returns role when role name exists`() {
+            // Arrange
+            val roleName = role.name
+
+            // Act
+            val actual: Role? = roleRepository.findByName(roleName)
+
+            // Assert
+            assertEquals(role, actual)
+        }
+
+        @Test
+        fun `returns role when role name doesn't exists`() {
+            // Arrange
+            val roleName = "NOT_FOUND_${role.name}"
+
+            // Act
+            val actual: Role? = roleRepository.findByName(roleName)
+
+            // Assert
+            assertNull(actual)
         }
     }
 }
