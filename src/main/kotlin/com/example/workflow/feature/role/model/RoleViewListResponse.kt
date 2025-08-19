d8ab8268d4655fafcb39339fe6533e4504ef1bcf
hiro <43287732+h10i@users.noreply.github.com>
2025-08-19 22:46:28 +0900

    chore(role): add schema annotation for OpenAPI docs

--- src/main/kotlin/com/example/workflow/feature/role/model/RoleViewListResponse.kt
+++ src/main/kotlin/com/example/workflow/feature/role/model/RoleViewListResponse.kt
@@ -1,5 +1,22 @@
 package com.example.workflow.feature.role.model
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class RoleViewListResponse(
+    @get:Schema(
+        description = "role list",
+        example = """
+            "roles": [
+                {
+                    "id": "58196e4f-62cf-4a89-817e-d54116a78a43",
+                    "name": "EXAMPLE_USER",
+                },
+                {
+                    "id": "9bffdd2e-0f25-4032-ab31-ab390192e283",
+                    "name": "EXAMPLE_ADMIN",
+                }
+            ]
+        """,
+    )
     val roles: List<RoleViewResponse>
 )
