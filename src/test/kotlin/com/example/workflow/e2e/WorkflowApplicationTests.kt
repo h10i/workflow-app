38fda339e242e4d3fa843bef555dd478caecd5df
hiro <43287732+h10i@users.noreply.github.com>
2025-09-18 00:02:40 +0900

    refactor: remove empty class body

--- src/test/kotlin/com/example/workflow/e2e/WorkflowApplicationTests.kt
+++ src/test/kotlin/com/example/workflow/e2e/WorkflowApplicationTests.kt
@@ -4,5 +4,4 @@ import com.example.workflow.e2e.test.base.AbstractE2ETest
 import com.example.workflow.support.annotation.E2ETest
 
 @E2ETest
-class WorkflowApplicationTests : AbstractE2ETest() {
-}
+class WorkflowApplicationTests : AbstractE2ETest()
