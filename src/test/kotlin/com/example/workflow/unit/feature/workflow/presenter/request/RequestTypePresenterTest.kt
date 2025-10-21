84212804113e54d137e56ccb8f7fc215c1f499be
hiro <43287732+h10i@users.noreply.github.com>
2025-10-21 19:44:22 +0900

    feat(workflow): add RequestTypePresenter

---
+++ src/test/kotlin/com/example/workflow/unit/feature/workflow/presenter/request/RequestTypePresenterTest.kt
@@ -0,0 +1,54 @@
+package com.example.workflow.unit.feature.workflow.presenter.request
+
+import com.example.workflow.feature.workflow.model.request.RequestTypeViewDto
+import com.example.workflow.feature.workflow.model.request.RequestTypeViewResponse
+import com.example.workflow.feature.workflow.model.request.toViewResponse
+import com.example.workflow.feature.workflow.presenter.request.RequestTypePresenter
+import com.example.workflow.support.annotation.UnitTest
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.unmockkStatic
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+@UnitTest
+class RequestTypePresenterTest {
+    private lateinit var requestTypePresenter: RequestTypePresenter
+
+    @BeforeEach
+    fun setUp() {
+        requestTypePresenter = RequestTypePresenter()
+    }
+
+    @Nested
+    inner class ToResponseFun {
+        @BeforeEach
+        fun setUp() {
+            mockkStatic(RequestTypeViewDto::toViewResponse)
+        }
+
+        @AfterEach
+        fun tearDown() {
+            unmockkStatic(RequestTypeViewDto::toViewResponse)
+        }
+
+        @Test
+        fun `should return a presenter result`() {
+            // Arrange
+            val requestTypeViewDto: RequestTypeViewDto = mockk()
+            val requestTypeViewResponse: RequestTypeViewResponse = mockk()
+
+            every { requestTypeViewDto.toViewResponse() } returns requestTypeViewResponse
+
+            // Act
+            val actual = requestTypePresenter.toResponse(requestTypeViewDto)
+
+            // Assert
+            assertEquals(requestTypeViewResponse, actual.response)
+        }
+    }
+}
